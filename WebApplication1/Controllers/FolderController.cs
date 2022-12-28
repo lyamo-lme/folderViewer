6facc3ef4ad03c1545680cbbd98ce777737a8381
maksem limont <ki212_lme@student.ztu.edu.ua>
2022-12-28 19:22:58 +0200

    created folder model, and folder contoller

---
+++ WebApplication1/Controllers/FolderController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+[Route("root")]
+public class FolderController : Controller
+{
+
+    public FolderController()
+    {
+    }
+
+    [Route("folder")]
+    public async Task<ActionResult<List<Folder>>> GetFolder(int folderId)
+    {
+        return View("Index", new List<Folder>());
+    }
+}
---
+++ WebApplication1/Models/Folder.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models;
+
+public class Folder
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int? ParentId { get; set; }
+    public List<Folder> Folders { get; set; }
+}
