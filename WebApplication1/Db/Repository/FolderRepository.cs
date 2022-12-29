0101915562f4b321302c87b5ff05c6e02523ecf5
maksem limont <ki212_lme@student.ztu.edu.ua>
2022-12-29 14:36:45 +0200

    added foler repository

---
+++ WebApplication1/Db/Repository/FolderRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Db.Repository;
+
+public class FolderRepository : IFolderRepository
+{
+    private DBContext DbContext { get; set; }
+
+    public FolderRepository(DBContext context)
+    {
+        DbContext = context;
+    }
+
+    public async Task<Folder> GetFolderAsync(Func<Folder, bool> funct)
+    {
+        try
+        {
+            await using (DbContext)
+            {
+                var model = await DbContext.Folders.FirstAsync(x => funct(x));
+                model.Folders = await DbContext.Folders.Where(x => x.ParentId == model.Id).ToListAsync();
+                return model;
+            }
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+
+    public async Task<Folder> AddFolderAsync(Folder folder)
+    {
+        await using (DbContext)
+        {
+            try
+            {
+                await DbContext.Folders.AddAsync(folder);
+                await DbContext.SaveChangesAsync();
+                return folder;
+            }
+            catch
+            {
+                throw new Exception("error to create folder");
+            }
+        }
+    }
+}
---
+++ WebApplication1/Db/Repository/IFolderRepository.cs
@@ -0,0 +1,9 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Db.Repository;
+
+public interface IFolderRepository
+{
+    public Task<Folder> GetFolderAsync(Func<Folder, bool> funct);
+    public Task<Folder> AddFolderAsync(Folder folder);
+}
